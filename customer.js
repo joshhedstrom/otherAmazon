93284bf8647225476390150a8707968bd70414e6
Josh Hedstrom <josh.hedstrom@gmail.com>
2018-05-07 13:26:45 -0500

    formatted code

--- customer.js
+++ customer.js
@@ -87,10 +87,13 @@ function getQuantity(name) {
         if (err) console.log(err)
         if (res) {
             quantityAvailable = res[0].stock_quantity;
-            if (quantitySelected < quantityAvailable) {
+            if (quantitySelected =< quantityAvailable) {
                 console.log('Order has been placed! Thank you for your business!')
                 let newStock = quantityAvailable - quantitySelected
                 updateQuantity(name, newStock)
+            }
+            else if(quantitySelected > quantityAvailable){
+                console.log("I'm sorry, but the quantity you requested is not available. Please check back later")
             }
         }
     })
